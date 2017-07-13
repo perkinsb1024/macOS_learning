a553fce417ad9bf1f04e707715a93d27645de1a2
Ben Perkins <bperkins@fitbit.com>
2017-07-13 09:29:17 -0700

    Added StormViewer Project 1

---
+++ StormViewer/StormViewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  StormViewer
+//
+//  Created by Ben Perkins on 7/13/17.
+//  Copyright © 2017 BenPerkins. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ StormViewer/StormViewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StormViewer
+//
+//  Created by Ben Perkins on 7/13/17.
+//  Copyright © 2017 BenPerkins. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
