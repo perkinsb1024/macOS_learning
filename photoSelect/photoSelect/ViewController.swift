8d2aa2605a9c98f88d184a74c7b0ceb561823614
Ben Perkins <bperkins@fitbit.com>
2017-08-08 18:17:32 -0700

    Project 7 can now save slideshows to video (finished)

--- photoSelect/photoSelect/ViewController.swift
+++ photoSelect/photoSelect/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import Cocoa
+import AVFoundation
 
 class ViewController: NSViewController, NSCollectionViewDelegate, NSCollectionViewDataSource {
 
@@ -153,6 +154,200 @@ class ViewController: NSViewController, NSCollectionViewDelegate, NSCollectionVi
             }
         }
         photoCollectionView.animator().deleteItems(at: photoCollectionView.selectionIndexPaths)
+    }
+    
+    func exportFinished(error: Error?) {
+        
+        let message: String
+        
+        if let error = error {
+            
+            message = "Error: \(error.localizedDescription)"
+            
+        } else {
+            
+            message = "Sucess!"
+        }
+        
+        let alert = NSAlert()
+        alert.messageText = message
+        alert.runModal()
+    }
+    
+    func createText(frame: CGRect) -> CALayer {
+        
+        //create a dictionary of text attributes
+        let attrs = [NSFontAttributeName : NSFont.boldSystemFont(ofSize: 24), NSForegroundColorAttributeName: NSColor.green]
+        
+        //combine those attributes with our message
+        let text = NSAttributedString(string: "PhotoSelect Project 7", attributes: attrs)
+        
+        //figure out how big the full string is
+        let textSize = text.size()
+        
+        //create the text layer
+        let textLayer = CATextLayer()
+        
+        //make the text layer the correct size
+        textLayer.bounds = CGRect(origin: CGPoint.zero, size: textSize)
+        
+        //make it align itself by its bottom-right corner
+        textLayer.anchorPoint = CGPoint(x: 1, y: 1)
+        
+        //position it just up from the bottom-right of the render frame
+        textLayer.position = CGPoint(x: frame.maxX - 10, y: textSize.height + 10)
+        
+        //give it the attributed string we created
+        textLayer.string = text
+        
+        //force it to render immediately
+        textLayer.display()
+        
+        //send it back to be added to the final render
+        return textLayer
+    }
+    
+    func createSlideShow(frame: CGRect, duration: CFTimeInterval) -> CALayer {
+        
+        //create the layer for our slideshow
+        let imageLayer = CALayer()
+        
+        //position it so it fills its space and is centered
+        imageLayer.bounds = frame
+        imageLayer.position = CGPoint(x: imageLayer.bounds.midX, y: imageLayer.bounds.midY)
+        
+        //make it stretch its content to fit
+        imageLayer.contentsGravity = kCAGravityResizeAspectFill
+        
+        //create a keyframe animation of the contents property
+        let fadeAnim = CAKeyframeAnimation(keyPath: "contents")
+        
+        //tell it to last as long as we need
+        fadeAnim.duration = duration
+        
+        //configure the properties
+        fadeAnim.isRemovedOnCompletion = false
+        fadeAnim.beginTime = AVCoreAnimationBeginTimeAtZero
+        
+        //prepare an array of all the NSImage objects we want to show
+        var values = [NSImage]()
+        
+        //loop through every photo, adding it twice se we're not constantly animating
+        for photo in photos {
+            
+            if let image = NSImage(contentsOfFile: photo.path) {
+                
+                values.append(image)
+                values.append(image)
+            }
+        }
+        //assign tha array to the animation
+        fadeAnim.values = values
+        
+        //then add the animation to the layer
+        imageLayer.add(fadeAnim, forKey: nil)
+        
+        return imageLayer
+    }
+    
+    func createVideoLayer(in parentLayer: CALayer, composition: AVMutableComposition, videoComposition: AVMutableVideoComposition, timeRange: CMTimeRange) -> CALayer {
+        
+        //create a layer for the video
+        let videoLayer = CALayer()
+        
+        //configure our post-processing animation tool
+        videoComposition.animationTool = AVVideoCompositionCoreAnimationTool(postProcessingAsVideoLayer: videoLayer, in: parentLayer)
+        
+        //prepare to add the black.mp4 video
+        let mutableCompositionVideoTrack = composition.addMutableTrack(withMediaType: AVMediaTypeVideo, preferredTrackID: kCMPersistentTrackID_Invalid)
+        
+        //find and load the black.mp4 video
+        let trackURL = Bundle.main.url(forResource: "black", withExtension: "mp4")!
+        let asset = AVAsset(url: trackURL)
+        
+        //pull out its video
+        let track = asset.tracks[0]
+        
+        //insert it into the track, filling all the time we need
+        try! mutableCompositionVideoTrack.insertTimeRange(timeRange, of: track, at: kCMTimeZero)
+        
+        //send the video layer back
+        return videoLayer
+    }
+    
+    func exportMovie(at size: NSSize) throws {
+        
+        //1 - 2 seconds per photo
+        let videoDuration = Double(photos.count * 2)
+        let timeRange = CMTimeRangeMake(kCMTimeZero, CMTimeMakeWithSeconds(videoDuration, 600))
+        
+        //2 - create a URL we can save our video to, then delete it if it already exists
+        let savePath = photosPath.appendingPathComponent("video.mp4")
+        let fm = FileManager.default
+        
+        if fm.fileExists(atPath: savePath.path) {
+            try fm.removeItem(at: savePath)
+        }
+        
+        //3 - create a composition for our entire render
+        let mutableComposition = AVMutableComposition()
+        
+        //4 - create a video composition for our post-processing video work (this is the only thing we're doing)
+        let videoComposition = AVMutableVideoComposition()
+        videoComposition.renderSize = size
+        videoComposition.frameDuration = CMTimeMake(1, 30)
+        
+        //5 - create a master CALayer that will hold all the child layers
+        let parentLayer = CALayer()
+        parentLayer.frame = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        
+        //6 - add all three child layers to the master layer
+        parentLayer.addSublayer(createVideoLayer(in: parentLayer, composition: mutableComposition, videoComposition: videoComposition, timeRange: timeRange))
+        parentLayer.addSublayer(createSlideShow(frame: parentLayer.frame, duration: videoDuration))
+        parentLayer.addSublayer(createText(frame: parentLayer.frame))
+        
+        //7 - create video rendering instructions saying how long a video we want
+        let instruction = AVMutableVideoCompositionInstruction()
+        instruction.timeRange = timeRange
+        videoComposition.instructions = [instruction]
+        
+        //8 - create an export session for our whole composition, requesting maximum quality
+        let exportSession = AVAssetExportSession(asset: mutableComposition, presetName: AVAssetExportPresetHighestQuality)!
+        
+        //9 - point the export session at the URL to our save file, pass it the post-processing work, and ask for an MPEG4 in return
+        exportSession.outputURL = savePath
+        exportSession.videoComposition = videoComposition
+        exportSession.outputFileType = AVFileTypeMPEG4
+        
+        //10 - start the export
+        exportSession.exportAsynchronously { [unowned self] in
+            
+            DispatchQueue.main.async {
+                //the export has finished - call "exportFinished()"
+                self.exportFinished(error: exportSession.error)
+            }
+        }
+    }
+    
+    @IBAction func runExport(_ sender: NSMenuItem) {
+        
+        let size: CGSize
+        
+        if sender.tag == 720 {
+            
+            size = CGSize(width: 1280, height: 720)
+            
+        } else {
+            
+            size = CGSize(width: 1920, height: 1080)
+        }
+        do {
+            try exportMovie(at: size)
+            
+        } catch {
+            
+            print("Error")
+        }
     }
 }
 
